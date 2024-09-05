b5dea760c6e3ff4596b1c30429deac183dc353c1
Casimiro Rocha <casimiroaf@gmail.com>
2024-09-05 02:48:55 -0300

    Glide user avatar

--- resources/views/components/img.blade.php
+++ resources/views/components/img.blade.php
@@ -4,10 +4,8 @@
     ...config('laraglide.img_params')
 ])
 
-<figure>
-    <img key="img-{{ $src }}"
-         src="{{ config('laraglide.base_url') }}/{{ $src }}?fit={{ $fit }}&w={{ $width }}&h={{ $height }}&dpr={{ $ratio }}&pixel={{ $pixel }}&blur={{ $blur }}&border={{ $borderSize }},{{ $borderColor }},{{ $borderType }}&q={{ $quality }}&gam={{ $gama }}&fm={{ $encodeFormat }}&con={{ $contrast }}&bri={{ $brightness }}&sharp={{ $sharp }}"
-         width="100%"
-         alt="{{ $alt }}"
-    />
-</figure>
+<img key="img-{{ $src }}"
+     {{ $attributes->merge(['class' => 'w-100']) }}
+     src="{{ config('laraglide.base_url') }}/{{ $src }}?fit={{ $fit }}&w={{ $width }}&h={{ $height }}&dpr={{ $ratio }}&pixel={{ $pixel }}&blur={{ $blur }}&border={{ $borderSize }},{{ $borderColor }},{{ $borderType }}&q={{ $quality }}&gam={{ $gama }}&fm={{ $encodeFormat }}&con={{ $contrast }}&bri={{ $brightness }}&sharp={{ $sharp }}"
+     alt="{{ $alt }}"
+/>

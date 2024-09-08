838bcfd9841969cd84338a3f32c0df48e716b1b1
Casimiro Rocha <casimiroaf@gmail.com>
2024-09-08 08:11:34 -0300

    ...

--- resources/views/components/img.blade.php
+++ resources/views/components/img.blade.php
@@ -4,7 +4,7 @@
     ...config('laraglide.img_params')
 ])
 
-<img key="img-{{ $src }}"
+<img wire:key="{{ $src }}"
      {{ $attributes->merge(['class' => 'w-100']) }}
      src="{{ config('laraglide.base_url') }}/{{ $src }}?fit={{ $fit }}&w={{ $width }}&h={{ $height }}&dpr={{ $ratio }}&pixel={{ $pixel }}&blur={{ $blur }}&border={{ $borderSize }},{{ $borderColor }},{{ $borderType }}&q={{ $quality }}&gam={{ $gama }}&fm={{ $encodeFormat }}&con={{ $contrast }}&bri={{ $brightness }}&sharp={{ $sharp }}"
      alt="{{ $alt }}"
